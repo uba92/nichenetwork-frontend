8f22dc7d1c01d66cdda7f713362d8eee3a731c83
Umberto Tramontano <u.tramontano92@libero.it>
2025-03-11 10:39:36 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>My first page</h1>
+      <div>
+        <ul>
+          <li>
+            <a href='/about'>About</a>
+          </li>
+        </ul>
+      </div>
+
+      <div>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
