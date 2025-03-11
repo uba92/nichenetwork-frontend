3fcaba05e0d95e0434675569cc030a5c00403489
Umberto Tramontano <u.tramontano92@libero.it>
2025-03-11 17:00:57 +0100

    finishes landing-page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,38 @@
 import { Button, Col, Container, Row } from 'react-bootstrap'
 import '../assets/css/LandingPage.css'
 import { Link } from 'react-router-dom'
+// eslint-disable-next-line no-unused-vars
+import { motion } from 'framer-motion'
 
 function LandingPage() {
   return (
     <Container fluid className='landing-container'>
-      <Row className='justify-content-center'>
+      <Row>
+        <Col>
+          <motion.div
+            initial={{ opacity: 0, y: 70 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 2 }}
+          >
+            <img
+              className='landing-img hide-mobile'
+              src='/img/cinema.jpg'
+              alt='cinema'
+            />
+            <img
+              className='landing-img'
+              src='/img/cinofilia.jpg'
+              alt='cinofilia'
+            />
+            <img
+              className='landing-img'
+              src='/img/equitazione.jpg'
+              alt='equitazione'
+            />
+          </motion.div>
+        </Col>
+      </Row>
+      <Row className='justify-content-center z-1'>
         <Col md='8' lg='6'>
           <h1 className='landing-title'>Benvenuto su NicheNetwork</h1>
           <p className='landing-sub'>
@@ -18,6 +45,23 @@ function LandingPage() {
               </Button>
             </Link>
           </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <motion.div
+            initial={{ opacity: 0, y: -70 }}
+            animate={{ opacity: 0.8, y: 0 }}
+            transition={{ duration: 2 }}
+          >
+            <img className='landing-img' src='/img/natura.jpg' alt='natura' />
+            <img
+              className='landing-img hide-mobile'
+              src='/img/sport.jpg'
+              alt='sport'
+            />
+            <img className='landing-img' src='/img/tech.jpg' alt='tech' />
+          </motion.div>
         </Col>
       </Row>
     </Container>
